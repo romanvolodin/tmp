4581537e9360d4b7ef15f8786af7038ef57cc061
Roman Volodin <roman.volodin@gmail.com>
2022-07-27 23:42:53 +0300

    revert echo options

--- create_google_credentials_json_from_env.sh
+++ create_google_credentials_json_from_env.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "------> Generating google-credentials.json from Heroku config var"
-echo -E $GOOGLE_CREDENTIALS > google-credentials.json
+echo $GOOGLE_CREDENTIALS > google-credentials.json
 echo "~~~~~~~~~~~~~~~~~~~~~"
 cat google-credentials.json
 echo "~~~~~~~~~~~~~~~~~~~~~"
