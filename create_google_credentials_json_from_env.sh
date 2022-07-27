f365a6c6de6092de1d05feb14fd1fb181590bba1
Roman Volodin <roman.volodin@gmail.com>
2022-07-27 22:44:30 +0300

    Add debug print for google-creds

--- create_google_credentials_json_from_env.sh
+++ create_google_credentials_json_from_env.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 echo "------> Generating google-credentials.json from Heroku config var"
 echo $GOOGLE_CREDENTIALS > google-credentials.json
+echo "~~~~~~~~~~~~~~~~~~~~~"
+cat google-credentials.json
+echo "~~~~~~~~~~~~~~~~~~~~~"
 exec "$@"
